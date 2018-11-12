1c9b44fb2a2a56827b0ac4db8ac827a40ddab219
MaryiaKhmialnitskaya <44965569+MaryiaKhmialnitskaya@users.noreply.github.com>
2018-11-12 15:31:25 +0300

    Update from config.py to config.ini

--- simple_app.py
+++ simple_app.py
@@ -1,6 +1,5 @@
 """
 -AdventureWorks2014. из query стоит удалить тк есть в connection_str и изменения имени бызв придется править в неск местах
--config ты сейчас используешь как dictionary, сделай его .ini файлом
 -вызови конфиг из мэйна через аргумент (например помести конфиг в другую папку и задай там другой запрос чтоб протестить)
 """
 
@@ -8,7 +7,7 @@ import logging.config
 import json 
 import os.path
 import pyodbc
-import config 
+import configparser
 
 def setup_logging(
     default_path='logging.json',
@@ -28,19 +27,21 @@ def setup_logging(
         logging.basicConfig(level=default_level)
 
 
-def connect_db():
+def connect_db(con_str):
 
-    cnxn = pyodbc.connect(config.DATABASE_CONFIG['connection_str'])
+    cnxn = pyodbc.connect(con_str)
     return cnxn
 
 def main(): 
+    config = configparser.ConfigParser()
+    config.read('config.ini')
     setup_logging()    
     logger = logging.getLogger(__name__)
     logger.info('Startlogging:')
 
     
     try:
-        cnxn=connect_db()
+        cnxn=connect_db(config.get("DATABASE_CONFIG",'connection_str'))
     
     except pyodbc.ProgrammingError as e:
         logger.logging.warning('There was a pyodbc warning.  This is the info we have about it: %s' %(e) )
@@ -48,7 +49,7 @@ def main():
     logger.info('Extracting table data  ')
     
     cursor = cnxn.cursor()
-    sql=config.SQL_CONFIG['sel_q']
+    sql=config.get("SQL_CONFIG",'sel_q')
     cursor.execute(sql)
     data=str(cursor.fetchall())
     
@@ -58,9 +59,9 @@ def main():
     
     logger.info('Saving table data to file ')
     
-    save_path = config.FILE_CONFIG['save_path']
-    name_of_file = config.FILE_CONFIG['name_of_file']
-    completeName = os.path.join(save_path, name_of_file+config.FILE_CONFIG['extension'])         
+    save_path = config.get("FILE_CONFIG",'save_path')
+    name_of_file = config.get("FILE_CONFIG",'name_of_file')
+    completeName = os.path.join(save_path, name_of_file+config.get("FILE_CONFIG",'extension'))         
     file1 = open(completeName, "w")
     file1.write(data)
     file1.close()
